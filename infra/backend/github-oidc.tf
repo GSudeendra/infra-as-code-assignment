4dac2a8e0887e8ce170fbd11314edbaa9e4523f0
GSudeendra <sudeendra.g@thoughtworks.com>
2025-07-17 22:00:10 +0530

    milestone3

--- infra/backend/github-oidc.tf
+++ infra/backend/github-oidc.tf
@@ -40,6 +40,17 @@ resource "aws_iam_role_policy" "github_actions" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      # KMS (scoped to all keys in this account/region)
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:TagResource", "kms:UntagResource", "kms:CreateKey", "kms:DescribeKey", "kms:EnableKeyRotation", "kms:PutKeyPolicy", "kms:GetKeyPolicy", "kms:GetKeyRotationStatus", "kms:ListKeys", "kms:ListAliases", "kms:DisableKeyRotation", "kms:DeleteKey", "kms:ListResourceTags"
+        ]
+        Resource = [
+          "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/*"
+        ]
+      },
+      # S3 (all project buckets)
       {
         Effect = "Allow"
         Action = [
@@ -47,49 +58,50 @@ resource "aws_iam_role_policy" "github_actions" {
           "s3:GetBucketLocation", "s3:GetBucketVersioning", "s3:CreateBucket",
           "s3:DeleteBucket", "s3:PutBucketVersioning", "s3:PutBucketEncryption",
           "s3:PutBucketAcl", "s3:PutBucketPolicy", "s3:GetBucketPolicy", "s3:DeleteBucketPolicy",
-          "s3:PutObjectAcl", "s3:GetObjectAcl", "s3:ListAllMyBuckets",
-          "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem",
-          "dynamodb:CreateTable", "dynamodb:DeleteTable", "dynamodb:DescribeTable",
-          "dynamodb:ListTables", "dynamodb:TagResource", "dynamodb:Scan", "dynamodb:UpdateItem",
-          "lambda:*",
-          "apigateway:*", "execute-api:*",
-          "logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents", "logs:DescribeLogGroups", "logs:DeleteLogGroup",
-          "cloudwatch:*", "cloudwatch:PutMetricAlarm", "cloudwatch:DeleteAlarms",
-          "kms:TagResource", "kms:UntagResource", "kms:CreateKey", "kms:DescribeKey", "kms:EnableKeyRotation", "kms:PutKeyPolicy", "kms:GetKeyPolicy", "kms:GetKeyRotationStatus", "kms:ListKeys", "kms:ListAliases", "kms:DisableKeyRotation", "kms:DeleteKey", "kms:ListResourceTags",
-          "xray:PutTraceSegments", "xray:PutTelemetryRecords",
-          "iam:GetPolicy", "iam:GetPolicyVersion", "iam:ListPolicyVersions", "iam:GetRolePolicy", "iam:ListRolePolicies", "iam:ListAttachedRolePolicies", "iam:ListPolicies", "iam:GetRole", "iam:CreateRole", "iam:DeleteRole", "iam:UpdateRole", "iam:PassRole", "iam:AttachRolePolicy", "iam:DetachRolePolicy", "iam:PutRolePolicy", "iam:DeleteRolePolicy", "iam:CreatePolicy", "iam:DeletePolicy", "iam:TagRole", "iam:TagPolicy", "iam:GetOpenIDConnectProvider"
+          "s3:PutObjectAcl", "s3:GetObjectAcl", "s3:ListAllMyBuckets"
         ]
         Resource = [
           "arn:aws:s3:::${var.project_prefix}-*",
           "arn:aws:s3:::${var.project_prefix}-*/*"
         ]
       },
+      # DynamoDB (all project tables)
       {
         Effect = "Allow"
         Action = [
           "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem",
           "dynamodb:CreateTable", "dynamodb:DeleteTable", "dynamodb:DescribeTable",
-          "dynamodb:ListTables", "dynamodb:TagResource"
+          "dynamodb:ListTables", "dynamodb:TagResource", "dynamodb:Scan", "dynamodb:UpdateItem"
         ]
         Resource = [
           "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.project_prefix}-*"
         ]
       },
+      # Lambda (all functions in this account/region)
       {
         Effect = "Allow"
         Action = ["lambda:*"]
         Resource = "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:*"
       },
+      # API Gateway (all)
       {
         Effect = "Allow"
         Action = ["apigateway:*", "execute-api:*"]
         Resource = "*"
       },
+      # CloudWatch Logs & CloudWatch (all)
       {
         Effect = "Allow"
         Action = ["logs:*", "cloudwatch:*"]
         Resource = "*"
       },
+      # X-Ray (all)
+      {
+        Effect = "Allow"
+        Action = ["xray:PutTraceSegments", "xray:PutTelemetryRecords"]
+        Resource = "*"
+      },
+      # IAM (least privilege for OIDC/role management)
       {
         Effect = "Allow"
         Action = [
